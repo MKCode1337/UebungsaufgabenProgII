371d0281cca0d250dbabad4bd938d689a3a0110a
MKCode1337 <max.b.kropf@gmail.com>
2026-03-18 12:49:58 +0100

    L14 Ü3

--- src/main/java/Adresse.java
+++ src/main/java/Adresse.java
@@ -1,10 +1,14 @@
 public class Adresse{
     String strasse;
-    int hausnummer;
+    String hausnummer;
     String postleitzahl;
     String ort;
 
-    public Adresse(String strasse, int hausnummer, String postleitzahl, String ort) {
+    public Adresse(String strasse, String hausnummer, String postleitzahl, String ort) {
+        if (strasse.charAt(0) > 'A' && strasse.charAt(0) < 'Z') throw new RuntimeException("Strassenname muss mit einem Großbuchgstaben beginnen");
+        if (ort.charAt(0) > 'A' && ort.charAt(0) < 'Z') throw new RuntimeException("Ortsname muss mit einem Großbuchgstaben beginnen");
+        if (hausnummer.charAt(0) > '0' && hausnummer.charAt(0) < '9') throw new RuntimeException("Ortsname muss mit einer Ziffer beginnen");
+
         this.strasse = strasse;
         this.hausnummer = hausnummer;
         this.postleitzahl = postleitzahl;
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -4,6 +4,8 @@ public class Person {
     Adresse adresse;
 
     public Person(String vorname, String nachname, Adresse adresse) {
+        if (vorname.charAt(0) > 'A' && vorname.charAt(0) < 'Z') throw new RuntimeException("Vorname muss mit einem Großbuchgstaben beginnen");
+
         this.vorname = vorname;
         this.nachname = nachname;
         this.adresse = adresse;
