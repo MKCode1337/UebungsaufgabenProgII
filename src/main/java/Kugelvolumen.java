2aac2713b515df4b72d68794753a83d9ca9b21b2
MKCode1337 <max.b.kropf@gmail.com>
2026-03-18 11:46:55 +0100

    Initial commit

---
+++ src/main/java/Kugelvolumen.java
@@ -0,0 +1,8 @@
+public class Kugelvolumen
+{
+    public static double berechneKugelvolumen(double radius)
+    {
+        if (radius < 0) throw new RuntimeException("Radius darf nicht negativ sein.");
+        return (4.0/3.0)*Math.PI*radius*radius*radius;
+    }
+}
