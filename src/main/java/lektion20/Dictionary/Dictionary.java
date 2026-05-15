94d3424894b6c17901f2d381d6e6b22c55bf061c
MKCode1337 <max.b.kropf@gmail.com>
2026-05-15 12:29:06 +0200

    L20 Üb3

--- src/main/java/lektion20/Dictionary/Dictionary.java
+++ src/main/java/lektion20/Dictionary/Dictionary.java
@@ -1,5 +1,6 @@
 package lektion20.Dictionary;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +9,18 @@ public class Dictionary {
     Map<String, List<String>> english2German = new HashMap<>();
 
     public void put(String engWord,String deWord) {
-        //Prüfe ob engWord schon vorhanden
-        //Füge neuen Eintrag ein
+        //Prüfe ob engWord schon vorhanden und füge neuen Eintrag ein
+        ArrayList<String> deWords = new ArrayList<>();
+        if (english2German.containsKey(engWord)) {
+            deWords = (ArrayList<String>) english2German.get(engWord);
+            deWords.add(deWord);
+            english2German.put(engWord,deWords);
+        }
         //ODER: Füge deWord zu Liste hinzu
+        else {
+            deWords.add(deWord);
+            english2German.put(engWord,deWords);
+        }
     }
     public List<String> get(String engWord) {
         return english2German.get(engWord);
