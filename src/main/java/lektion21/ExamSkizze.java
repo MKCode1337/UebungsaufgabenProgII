e3b6d62c8a24e09204501bd427096005e76fe6d6
MKCode1337 <max.b.kropf@gmail.com>
2026-05-28 11:53:06 +0200

    L21 Exam D)

--- src/main/java/lektion21/ExamSkizze.java
+++ src/main/java/lektion21/ExamSkizze.java
@@ -12,20 +12,31 @@ public class ExamSkizze {
     private void readQuestions() throws IOException{}
     private void toTest() throws IOException {}
 
-    public class ExamBuilder {
+    public static class ExamBuilder {
         private HashSet<Exam.Question> questions;
-        String questionsFile;
-        String testFile;
-        public ExamBuilder(String questionsFile, String testFile){
+
+        public ExamBuilder(){
             this.questions = new HashSet<>();
-            this.questionsFile = questionsFile;
-            this.testFile = testFile;
         }
-        public Exam build() throws IOException {
-            Exam exam = new Exam();
-            exam.readQuestions();
-            exam.toTest();
-            return exam;
+
+        public ExamBuilderTest readQuestions(String questions){
+                readQuestions(questions);
+                return new ExamBuilderTest();
         }
+
+        public class ExamBuilderTest{
+            private HashSet<Exam.Question> questions;
+            public ExamBuilderTest toTest(String testFile){
+                toTest(testFile);
+                return this;
+            }
+            public Exam build(){
+                return new Exam();
+            }
+        }
+    }
+
+    static void main() throws IOException {
+        Exam exam = new ExamBuilder().readQuestions("questions.csv").toTest("test.tex").build();
     }
 }
