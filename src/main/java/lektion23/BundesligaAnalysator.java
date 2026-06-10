d179f3ec6e295ba8f3c0c49956147bb78fe77661
MKCode1337 <max.b.kropf@gmail.com>
2026-06-10 08:06:15 +0200

    L23 Ü1 b)

--- src/main/java/lektion23/BundesligaAnalysator.java
+++ src/main/java/lektion23/BundesligaAnalysator.java
@@ -1,9 +1,14 @@
 package lektion23;
 
 import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Arrays.compare;
 
 public class BundesligaAnalysator {
     static void main() {
         List<Mannschaft> bundesligaTabelle = Mannschaft.createTabelle();
+        System.out.println("Alle Mannschaften mit mehr als 50 Punkten: ");
+        bundesligaTabelle.stream().filter(mannschaft -> mannschaft.punkte > 50).forEach(mannschaft -> System.out.println(mannschaft.punkte));
     }
 }
