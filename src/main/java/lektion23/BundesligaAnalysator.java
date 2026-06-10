9851f2829236b8cebc7f9287fbbe3633b0369e06
MKCode1337 <max.b.kropf@gmail.com>
2026-06-10 07:53:24 +0200

    L23 Ü1a)

---
+++ src/main/java/lektion23/BundesligaAnalysator.java
@@ -0,0 +1,9 @@
+package lektion23;
+
+import java.util.List;
+
+public class BundesligaAnalysator {
+    static void main() {
+        List<Mannschaft> bundesligaTabelle = Mannschaft.createTabelle();
+    }
+}
